87a7f6d527848a0b3a546fc6b432fe15698d9e83
Mista Tea <thatguythomas@hotmail.com>
2015-11-04 02:02:40 -0700

    Adding example files

---
+++ lua/examples/example_phrases.lua
@@ -0,0 +1,8 @@
+localify.Bind( "en", "@Health", "Health" )
+localify.Bind( "es", "@Health", "Salud" )
+
+localify.Bind( "en", "!Volume!", "Volume" )
+localify.Bind( "de", "!Volume!", "Volumen" )
+
+localify.Bind( "en", "#Shield", "Shield" )
+localify.Bind( "he", "#Shield", "מָגֵן" )
